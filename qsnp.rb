ecb1e283bbcd20bfeba4bf097a0f38207be9831a
Lorena <lorena.pantano@gmail.com>
2014-11-12 15:28:57 -0500

    add qsnp to brew

---
+++ qsnp.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+class Qsnp < Formula
+  homepage 'http://qcmg.org/bioinformatics/tiki-index.php?page=qSNP'
+  version '1.0'
+  url 'http://qcmg.org/bioinformatics/tiki-download_file.php?fileId=27'
+
+  def install
+    java = share / 'java'
+    java.install Dir['*.jar']
+    bin.mkdir
+    open(bin / 'qsnp', 'w') do |file|
+      file.write <<-EOS.undent
+        #!/bin/bash
+        default_jvm_mem_opts="-Xms750m -Xmx2g"
+        jvm_mem_opts=""
+        jvm_prop_opts=""
+        pass_args=""
+        for arg in "$@"; do
+            case $arg in
+                '-D'*)
+                    jvm_prop_opts="$jvm_prop_opts $arg"
+                    ;;
+                 '-Xm'*)
+                    jvm_mem_opts="$jvm_mem_opts $arg"
+                    ;;
+                 '*8')
+                    pass_args="$pass_args \\*8"
+                    ;;
+                 *)
+                    pass_args="$pass_args $arg"
+                    ;;
+            esac
+        done
+        if [ "$jvm_mem_opts" == "" ]; then
+            jvm_mem_opts="$default_jvm_mem_opts"
+        fi
+        eval java $jvm_mem_opts $jvm_prop_opts -cp #{java}/qsnp-#{version}.jar $pass_args
+      EOS
+    end
+  end
+
+  test do
+    system 'qsnp -h'
+  end
+end
